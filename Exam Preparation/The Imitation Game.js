a84e20bf302f1728dfd031ea6f304e6345ffeb0b
LeGrandSolo <snowblybg@gmail.com>
2022-04-09 16:44:10 +0300

    Added new files.

--- Exam Preparation/The Imitation Game.js
+++ Exam Preparation/The Imitation Game.js
@@ -1,20 +1,22 @@
 function imitationGame(input) {
-    let secretMessage = input.shift();
-    let currCommand = input.shift();
-    while (currCommand != 'Decode') {
-        let tokens = currCommand.split('|');
-        let [commandName, p1, p2] = tokens;
+    let secretMess = input.shift();
+    let command = input.shift();
+    while (command != 'Decode') {
+        let [commandName, p1, p2] = command.split('|');
         if (commandName == 'Move') {
-            secretMessage = secretMessage.substring(p1) + secretMessage.substring(0, p1);
+            p1 = Number(p1);
+            secretMess = secretMess.substring(p1) + secretMess.substring(0, p1);
         } else if (commandName == 'Insert') {
-            secretMessage = secretMessage.substring(0, p1) + p2 + secretMessage.substring(p1);
+            p1 = Number(p1);
+            secretMess = secretMess.substring(0, p1) + p2 + secretMess.substring(p1);
         } else if (commandName == 'ChangeAll') {
-            let regex = new RegExp(p1, 'g');
-            secretMessage = secretMessage.replace(regex, p2);
+            while (secretMess.includes(p1)) {
+                secretMess = secretMess.replace(p1, p2);
+            }
         }
-        currCommand = input.shift();
+        command = input.shift()
     }
-    console.log(`The decrypted message is: ${secretMessage}`);
+    console.log('The decrypted message is: ' + secretMess);
 }
 imitationGame([
     'zzHe',
