fa04e4af0936923ae18e841a9a749eb777c23646
LeGrandSolo <snowblybg@gmail.com>
2022-06-05 02:48:12 +0300

    Solved problems.

--- JS Advanced - May 2022/04. DOM Introduction/08. Generate Report/report.js
+++ JS Advanced - May 2022/04. DOM Introduction/08. Generate Report/report.js
@@ -1,3 +1,23 @@
 function generateReport() {
-    //TODO
+    let checkBoxes = document.querySelectorAll('thead tr th input');
+    let indexesOfCheckedAndNames = {};
+    for (let i = 0; i < checkBoxes.length; i++) {
+        if (checkBoxes[i].checked) {
+            indexesOfCheckedAndNames[checkBoxes[i].parentElement.textContent] = i;
+        }
+    }
+    let rows = document.querySelectorAll('tbody tr');
+    let objects = [];
+    for (const row of rows) {
+        let colms = row.querySelectorAll('td');
+        let newObj = {};
+        for (const property in indexesOfCheckedAndNames) {
+            newObj[property.toLowerCase().trim()] = colms[indexesOfCheckedAndNames[property]].textContent;
+        }
+        if (Object.keys(newObj).length > 0) {
+            objects.push(newObj);
+        }
+    }
+    let output = document.getElementById('output');
+    output.textContent = JSON.stringify(objects);
 }
--- JS Advanced - May 2022/04. DOM Introduction/09. Number Convertor/solution.js
+++ JS Advanced - May 2022/04. DOM Introduction/09. Number Convertor/solution.js
@@ -1,4 +1,54 @@
 function solve() {
-
-    //TODO...
+    document.querySelector('button').addEventListener('click', convert);
+    const options = document.querySelector('#selectMenuTo');
+    let optionBinery = document.createElement('option');
+    let optionHexa = document.createElement('option');
+    [optionBinery.value, optionBinery.textContent] = ['binary' , 'Binary'];
+    console.log(optionBinery.value);
+    [optionHexa.value, optionHexa.textContent] = ['hexadecimal', 'Hexadecimal'];
+    options.appendChild(optionBinery);
+    options.appendChild(optionHexa);
+    optionBinery.id = 'optBin';
+    optionHexa.id = 'optHex';
+    function convert(e) {
+        let decimalNum = Number(document.querySelector('input').value);
+        let optionBinery = document.querySelector('#optBin');
+        let optionHexa = document.querySelector('#optHex');
+        let output = document.getElementById('result');
+        if (optionBinery.selected) {
+            let binaryNum = '';
+            while (decimalNum >= 1) {
+                binaryNum += decimalNum % 2;
+                decimalNum = Math.floor(decimalNum / 2);
+            }
+            output.value = binaryNum.split('').reverse().join('');
+        }
+        if (optionHexa.selected) {
+            let hecaNum = '';
+            let correspLett = {
+                10: 'A',
+                11: 'B',
+                12: 'C',
+                13: 'D',
+                14: 'E',
+                15: 'F'
+            }
+            while (decimalNum / 16 >= 1) {
+                if (decimalNum % 16 > 9) {
+                    hecaNum += correspLett[decimalNum % 16];
+                } else if (decimalNum % 16 <= 9 && decimalNum % 16 !== 0) {
+                    hecaNum += decimalNum % 16;
+                } else {
+                    hecaNum += 0;
+                }
+                decimalNum = Math.floor(decimalNum / 16);
+            }
+            if (decimalNum % 16 > 9) {
+                hecaNum += correspLett[decimalNum % 16];
+            } else if (decimalNum % 16 <= 9 && decimalNum % 16 !== 0) {
+                hecaNum += decimalNum % 16;
+            }
+            output.value = hecaNum.split('').reverse().join('');
+        }
+    }
 }
